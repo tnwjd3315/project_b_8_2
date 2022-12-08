4daef4290773016c9466608f70e3a69aad0f4f15
Sujeong Kim <miysj3315@gmail.com>
2022-12-08 20:38:34 +0900

    수정 기능 patch로 구현 완료

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, jsonify, session
+from flask import Flask, render_template, request, jsonify, session, redirect, url_for
 import pymysql
 import json
 
@@ -66,34 +66,56 @@ def get_comments():
     db.close()
     return json_str,200
 
-@app.route("/edit")
-def edit_comment_page():
-    return render_template('edit.html')
+# @app.route("/edit")
+# def edit_comment_page():
+#     return render_template('edit.html')
+#
 
-@app.route("/comment", methods=["UPDATE"])
-def update_comment():
-
-    db = pymysql.connect(host='hjdb.cmux79u98wpg.us-east-1.rds.amazonaws.com', user='master', password='Abcd!234', db="hjdb", port=3306)
+@app.route("/edit/<int:comment_id>", methods=["GET", "PATCH"])
+def update_comment(comment_id):
+    db = pymysql.connect(host='hjdb.cmux79u98wpg.us-east-1.rds.amazonaws.com', user='master', password='Abcd!234',
+                         db="hjdb", port=3306)
     cursor = db.cursor()
 
-    comment_title = request.form["comment_title"]
-    comment_content = request.form["comment_content"]
-    star = request.form["star"]
-    comment_id = request.form["comment_id"]
+    sql = """
+        SELECT comment.comment_id, comment.comment_title, users.user_id,comment.comment_content,comment.star, comment.user_unique_id,comment.problem_id, comment.review_id
+        FROM comment
+        INNER JOIN users ON comment.user_unique_id =users.unique_id;
+        """
+    cursor.execute(sql)
+    rows = cursor.fetchall()
+    # print(rows)
+    #
+    # comment_title = rows[-1][1]
+    # comment_content = rows[-1][3]
+    # star = rows[-1][4]
 
-    sql = """UPDATE comment
-	set comment_content = (%s), comment_title = (%s),star = (%s)
-	where comment_id = (%s)"""
+    json_str = json.dumps(rows, indent=4, sort_keys=True, default=str, ensure_ascii=False)
 
+    # db.commit()
+    # db.close()
+    # return json_str,200
 
+    if request.method == "PATCH":
+        print(request.get_json())
+        print("update start")
+        comment_title = request.get_json().get("comment_title")
+        comment_content = request.get_json().get("comment_content")
+        star = request.get_json().get("star")
+        # comment_id = request.get_json().get("comment_id")
+        # print(comment_id)
 
-	# where user_unique_id = '$(sessionID)'
-    # user_unique_id 대신 user_id를 가져오는 방법?
+        sql = "UPDATE comment set comment_content = %s, comment_title = %s,star = %s where comment_id = %s"
+        # where user_unique_id = '$(sessionID)'
+        # user_unique_id 대신 user_id를 가져오는 방법?
 
-    cursor.execute(sql, (comment_content, comment_title, star, comment_id))
-    db.commit()
-    db.close()
-    return jsonify({'msg': '댓글이 정상적으로 수정되었습니다.'})
+        cursor.execute(sql, (comment_content, comment_title, star, comment_id))
+        db.commit()
+        db.close()
+        return render_template('comment.html')
+
+    return render_template('edit.html')
+
 
 @app.route("/comment", methods=["DELETE"])
 def delete_comment():
